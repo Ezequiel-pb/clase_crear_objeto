bd119713ee7e09d4456c3353ed4676b66c5193a2
jhoan <jhoan@LAPTOP-SDIEDSBR>
2025-01-17 18:45:00 -0400

    Agregar archivos de proyecto.

---
+++ clase_crear objeto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Persona
+{
+
+    public string Nombre;
+    public int Edad;
+
+        public void Saludar()
+    {
+
+        Console.WriteLine($"Hola, soy {Nombre} y tengo {Edad} años.");
+    }
+
+    
+}
+
+class Program
+{
+    static void Main()
+    {
+        Persona persona1 = new Persona();
+        persona1.Nombre = "carlos";
+        persona1.Edad = 30;
+        persona1.Saludar();
+
+
+    }
+    
+}
